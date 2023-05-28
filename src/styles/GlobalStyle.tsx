2f1da71e31b5d9e1149bb6e1bfdfebab3031ee7d
KKamjangE <ajm980718@gmail.com>
2023-05-28 16:35:39 +0900

    Design: 전체적인 색상 변수 코드 수정

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -5,22 +5,18 @@ const GlobalStyle = createGlobalStyle`
     ${reset}
     :root{
         /** 색상 변수 */
-        --primary-100:#0085ff;
-        --primary-200:#69b4ff;
-        --primary-300:#e0ffff;
-        --accent-100:#006fff;
-        --accent-200:#e1ffff;
-        --text-100:#FFFFFF;
-        --text-200:#9e9e9e;
-        --bg-100:#1E1E1E;
-        --bg-200:#2d2d2d;
-        --bg-300:#454545;
+        --accent-yellow:#f8cd07;
+        --text-black:#000000;
+        --text-white:#ffffff;
+        --bg-white:#ffffff;
+        --bg-dark-gray:#202224;
+        --bg-light-gray:#f5f5f5;
 
         /** 스타일 변수 */
         font-family: 'Noto Sans KR', sans-serif;
         font-size: 24px;
-        background-color: var(--bg-100);
-        color: var(--text-100);
+        background-color: var(--bg-white);
+        color: var(--text-black);
     }
 `;
 
