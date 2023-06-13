c93fd4380c40552221c60b4349852e0b8571153e
KKamjangE <ajm980718@gmail.com>
2023-06-13 17:03:23 +0900

    Feat: 스크롤 프로그래스바

---
+++ src/components/contents/ProgressBar.tsx
@@ -0,0 +1,22 @@
+import { motion, useScroll } from "framer-motion";
+import styled from "styled-components";
+
+export default function ProgressBar() {
+  const { scrollYProgress } = useScroll();
+  return (
+    <ProgressBarStyled
+      className="progress-bar"
+      style={{ scaleX: scrollYProgress }}
+    />
+  );
+}
+
+const ProgressBarStyled = styled(motion.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 5px;
+  background-color: var(--accent-yellow);
+  transform-origin: 0%;
+`;
