f6f434213a4a0c0195a8f9924d40865b0f7549a2
KKamjangE <ajm980718@gmail.com>
2023-06-13 20:48:54 +0900

    Rename: ProgressBar 컴포넌트 motions 폴더로 이동

--- src/components/motions/index.ts
+++ src/components/motions/index.ts
@@ -1,3 +1,4 @@
 export { default as FadeInContent } from "./FadeInContent";
 export { default as FadeInDownContent } from "./FadeInDownContent";
 export { default as HoverLink } from "./HoverLink";
+export { default as ProgressBar } from "./ProgressBar";
