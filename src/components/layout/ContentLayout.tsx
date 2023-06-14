0ee10f2e55a642f92bdccd6de05e332201a9f4a6
KKamjangE <ajm980718@gmail.com>
2023-06-14 16:43:21 +0900

    Feat: ContentLayout 컴포넌트 Props 추가 및 레이아웃 수정

--- src/components/layout/ContentLayout.tsx
+++ src/components/layout/ContentLayout.tsx
@@ -1,14 +1,28 @@
 import React from "react";
 import styled from "styled-components";
+import YellowPoint from "./../motions/YellowPoint";
 
 export default function ContentLayout({
   children,
+  title,
 }: {
   children: React.ReactNode;
+  title?: string;
 }) {
-  return <ContentLayoutStyled>{children}</ContentLayoutStyled>;
+  return (
+    <ContentLayoutStyled>
+      <h1>
+        <YellowPoint>{title}</YellowPoint>
+      </h1>
+      {children}
+    </ContentLayoutStyled>
+  );
 }
 
 const ContentLayoutStyled = styled.div`
   padding: 100px 100px 50px 100px;
+  h1 {
+    font-size: 2rem;
+    font-weight: bold;
+  }
 `;
