785cddfb973fca077c8a79b1555c86ba06306959
KKamjangE <ajm980718@gmail.com>
2023-06-14 20:47:31 +0900

    Feat: Loading 컴포넌트 모듈 export

--- src/components/motions/index.ts
+++ src/components/motions/index.ts
@@ -3,3 +3,4 @@ export { default as FadeInDownContent } from "./FadeInDownContent";
 export { default as HoverLink } from "./HoverLink";
 export { default as ProgressBar } from "./ProgressBar";
 export { default as YellowPoint } from "./YellowPoint";
+export { default as Loading } from "./Loading";
