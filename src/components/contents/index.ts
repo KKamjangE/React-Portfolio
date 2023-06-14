082403b58f85c93bf9ca40d6e033dcaaedc90c7c
KKamjangE <ajm980718@gmail.com>
2023-06-14 21:51:01 +0900

    Feat: Project 컴포넌트 생성

--- src/components/contents/index.ts
+++ src/components/contents/index.ts
@@ -1,4 +1,5 @@
-export { default as Intro } from "./Intro";
-export { default as Work } from "./Work";
-export { default as Footer } from "./Footer";
 export { default as NavBar } from "./NavBar";
+export { default as Intro } from "./Intro";
+export { default as Footer } from "./Footer";
+export { default as Work } from "./Work";
+export { default as Project } from "./Project";
