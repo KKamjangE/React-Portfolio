a72b61f86243b3dd2330411b986844d2f83bad97
KKamjangE <ajm980718@gmail.com>
2023-06-16 14:56:53 +0900

    Feat: firebase 데이터 베이스와 연동

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,31 +1,11 @@
 import axios from "axios";
-import type {
-  WorkTypes,
-  ProjectTypes,
-  EducationTypes,
-  ShareTypes,
-} from "./types";
+import type { getDataTypes } from "./types";
 
 const axiosAPI = axios.create({
-  baseURL: "http://localhost:5173/src/data",
+  baseURL: import.meta.env.VITE_AXIOS_API_BASE_URL,
 });
 
-export const getWorkData = async <T = WorkTypes>(): Promise<T> => {
-  const response = await axiosAPI.get<T>("work.json");
-  return response.data;
-};
-
-export const getProjectData = async <T = ProjectTypes>(): Promise<T> => {
-  const response = await axiosAPI.get<T>("project.json");
-  return response.data;
-};
-
-export const getShareData = async <T = ShareTypes>(): Promise<T> => {
-  const response = await axiosAPI.get<T>("share.json");
-  return response.data;
-};
-
-export const getEducationData = async <T = EducationTypes>(): Promise<T> => {
-  const response = await axiosAPI.get<T>("education.json");
+export const getData = async <T = getDataTypes>(): Promise<T> => {
+  const response = await axiosAPI.get<T>("/data.json");
   return response.data;
 };
