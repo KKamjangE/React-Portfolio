a9c231a4429587d01ac95e2872dd4c354a0b0c7c
KKamjangE <ajm980718@gmail.com>
2023-06-20 20:28:06 +0900

    Design: 로딩 컴포넌트 디자인 수정

--- src/components/interactive/Loading.tsx
+++ src/components/interactive/Loading.tsx
@@ -1,15 +1,17 @@
-import { Dna } from "react-loader-spinner";
+import { Oval } from "react-loader-spinner";
 import styled from "styled-components";
 
 export default function Loading() {
   return (
     <LoadingStyled>
       <p>Loading...</p>
-      <Dna
+      <Oval
         visible={true}
         ariaLabel="정보를 불러오는 중입니다."
         height={100}
         width={100}
+        color="#f8cd07"
+        secondaryColor="#202224"
       />
     </LoadingStyled>
   );
@@ -24,6 +26,6 @@ const LoadingStyled = styled.div`
   p {
     font-size: 0.8rem;
     opacity: 0.6;
-    margin-bottom: 10px;
+    margin-bottom: 50px;
   }
 `;
