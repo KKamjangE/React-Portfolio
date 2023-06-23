35f13833cfad0edd2160f120c0da325a96b37013
KKamjangE <ajm980718@gmail.com>
2023-06-23 18:42:17 +0900

    Feat: Fade in 애니메이션 적용 및 폰트 굵기 수정

--- src/components/contents/skills/SkillDetail.tsx
+++ src/components/contents/skills/SkillDetail.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { useSkillsStore } from "@/store/store";
 import { motion, AnimatePresence } from "framer-motion";
 import { IoIosArrowDown } from "react-icons/io";
+import { FadeInContent } from "@/components";
 
 export default function SkillDetail({ skillName }: { skillName: string }) {
   const { skills } = useSkillsStore();
@@ -10,40 +11,42 @@ export default function SkillDetail({ skillName }: { skillName: string }) {
   const skillKey = skillName.split(" ").join("").toLowerCase();
 
   return (
-    <SkillDetailStyled>
-      <p className="skill-title" onClick={() => setIsOpen(!isOpen)}>
-        {skillName} <IoIosArrowDown data-open={isOpen} />
-      </p>
-      <AnimatePresence initial={false}>
-        {isOpen && (
-          <motion.p
-            key="detail"
-            initial="close"
-            animate="open"
-            exit="close"
-            variants={{
-              open: {
-                height: "auto",
-                paddingTop: "5px",
-                paddingBottom: "15px",
-              },
-              close: { height: 0, paddingTop: 0, paddingBottom: 0 },
-            }}
-            transition={{ duration: 0.2 }}
-            style={{ overflow: "hidden" }}
-            className="detail"
-          >
-            {skills?.data[skillKey]}
-          </motion.p>
-        )}
-      </AnimatePresence>
-    </SkillDetailStyled>
+    <FadeInContent>
+      <SkillDetailStyled>
+        <p className="skill-title" onClick={() => setIsOpen(!isOpen)}>
+          {skillName} <IoIosArrowDown data-open={isOpen} />
+        </p>
+        <AnimatePresence initial={false}>
+          {isOpen && (
+            <motion.p
+              key="detail"
+              initial="close"
+              animate="open"
+              exit="close"
+              variants={{
+                open: {
+                  height: "auto",
+                  paddingTop: "5px",
+                  paddingBottom: "15px",
+                },
+                close: { height: 0, paddingTop: 0, paddingBottom: 0 },
+              }}
+              transition={{ duration: 0.2 }}
+              style={{ overflow: "hidden" }}
+              className="detail"
+            >
+              {skills?.data[skillKey]}
+            </motion.p>
+          )}
+        </AnimatePresence>
+      </SkillDetailStyled>
+    </FadeInContent>
   );
 }
 
 const SkillDetailStyled = styled.div`
-  box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.2);
-  border: 1px solid rgba(0, 0, 0, 0.1);
+  /* border: 1px solid rgba(0, 0, 0, 0.1); */
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
   svg {
     transition: 0.2s;
   }
@@ -55,17 +58,18 @@ const SkillDetailStyled = styled.div`
   }
   p {
     font-size: 0.7rem;
-    font-weight: 300;
     background-color: var(--bg-white);
   }
   p.skill-title {
     display: flex;
     justify-content: space-between;
     padding: 15px;
+    font-weight: 400;
     cursor: pointer;
   }
   p.detail {
     padding: 0 15px;
     line-height: 1.2rem;
+    font-weight: 300;
   }
 `;
