981598c071b2e7d0f95371a3f84f623a5bdb7bc1
KKamjangE <ajm980718@gmail.com>
2023-06-24 22:52:16 +0900

    Fix: Provider 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Provider } from "react-redux";
-import { store } from "@/store";
 import Home from "@/pages/Home";
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <Home />
-    </Provider>
-  );
+  return <Home />;
 }
