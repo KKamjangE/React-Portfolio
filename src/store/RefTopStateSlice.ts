f591d9e4eb51f58e72752304d001d9f85425afb0
KKamjangE <ajm980718@gmail.com>
2023-07-12 21:58:49 +0900

    Feat: RefTop 배열 저장하는 Reducer 구현

---
+++ src/store/RefTopStateSlice.ts
@@ -0,0 +1,23 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+interface RefTopStateType {
+  RefTopArray: number[];
+}
+
+const initialState: RefTopStateType = {
+  RefTopArray: [],
+};
+
+const RefTopStateSlice = createSlice({
+  name: "RefTopState",
+  initialState,
+  reducers: {
+    setRefTop: (state, action: PayloadAction<number[]>) => {
+      state.RefTopArray = action.payload;
+    },
+  },
+});
+
+export const RefTopStateAction = RefTopStateSlice.actions;
+export const RefTopState = RefTopStateSlice.name;
+export const RefTopStateReducer = RefTopStateSlice.reducer;
