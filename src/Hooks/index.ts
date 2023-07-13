4a622a2f6595e9c1f90a67a915b8d75381b4a6ed
KKamjangE <ajm980718@gmail.com>
2023-07-13 14:20:31 +0900

    Chore: 빌드 문제 해결을 위한 git 캐시 삭제

--- src/Hooks/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as useAxiosGetContents } from "@/hooks/useAxiosGetContents";
-export { default as useAxiosGetSkillDesc } from "@/hooks/useAxiosGetSkillDesc";
-export { default as useCurrViewContNum } from "@/hooks/useCurrViewContNum";
-export { default as useGetContents } from "@/hooks/useGetContents";
-export { default as useGetRefOffsetList } from "@/hooks/useGetRefOffsetList";
-export { default as useGetSkillDesc } from "@/hooks/useGetSkillDesc";
-export { default as useRefsAtOffsets } from "@/hooks/useRefsAtOffsets";
-export { default as useSetRefOffsetList } from "@/hooks/useSetRefOffsetList";
--- src/api/index.ts
+++
@@ -1,6 +0,0 @@
-export { getContentsData as getContentsData } from "./axios";
-export { getSkillsData as getSkillsData } from "./axios";
-export type { ResponseData as ResponseData } from "./types";
-export type { ContentDataType as ContentDataType } from "./types";
-export type { UrlType as UrlType } from "./types";
-export type { SkillDescType as SkillDescType } from "./types";
