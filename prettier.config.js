12cc0d6e84c33ab2d3766889a93213e2dca2a00a
KKamjangE <ajm980718@gmail.com>
2024-12-18 14:14:41 +0900

    prettier config 파일 생성성

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+export default {
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+  printWidth: 100,
+  tabWidth: 4,
+  semi: false,
+  singleQuote: true,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "auto",
+};
