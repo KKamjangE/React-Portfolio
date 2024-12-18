341ede4edc3e50a858e27b094594252163e1952a
KKamjangE <ajm980718@gmail.com>
2024-12-18 14:34:58 +0900

    alias 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: { find: "@", replacement: resolve(__dirname, "src") },
-  },
-  plugins: [react(), tsconfigPaths()],
-});
+    resolve: {
+        alias: [{ find: '@', replacement: '/src' }],
+    },
+})
