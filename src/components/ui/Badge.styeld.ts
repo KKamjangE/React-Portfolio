2de24fa416f0d4e708609985ec5d0ce73461819b
KKamjangE <ajm980718@gmail.com>
2024-12-20 10:41:04 +0900

    Badge 컴포넌트 색상 hsl 변환 함수로 변경

---
+++ src/components/ui/Badge.styeld.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+const stringToHslColor = (str: string): string => {
+    let hash = 0
+    Array.from(str).forEach((s) => {
+        hash = s.charCodeAt(0) + ((hash << 5) - hash)
+    })
+    const h = hash % 360
+    const s = 70 + (hash % 30)
+    const l = 65 + (hash % 20)
+    return `hsl(${h}, ${s}%, ${l}%)`
+}
+
+const getTextColor = (bgColor: string): string => {
+    const l = parseInt(bgColor.match(/\d+%\)$/)![0])
+    return l > 60 ? '#000' : '#fff'
+}
+
+export const Badge = styled.span<{ text: string }>`
+    background-color: ${({ text }) => {
+        console.log(stringToHslColor(text))
+        return stringToHslColor(text)
+    }};
+    border-radius: 12px;
+    padding: 6px 10px;
+    font-size: 0.7rem;
+    font-weight: bold;
+    color: ${({ text }) => getTextColor(stringToHslColor(text))};
+`
