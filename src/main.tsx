4369374b96af44cd0bdd0e71c0a2b3d5ed0444dd
KKamjangE <ajm980718@gmail.com>
2024-12-28 15:45:51 +0900

    Analytics 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import App from '@/App.tsx'
 import GlobalStyle from '@/styles/GlobalStyle.tsx'
 import { Provider } from 'react-redux'
 import { store } from './store'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <Provider store={store}>
         <GlobalStyle />
         <App />
+        <Analytics />
     </Provider>
 )
