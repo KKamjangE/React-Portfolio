1a7b9b8b2caa98cda415a9d67ab68120614f9eea
KKamjangE <ajm980718@gmail.com>
2024-12-30 10:14:58 +0900

    YellowPoint 컴포넌트명 수정정

--- src/components/motion/HoverMotion.tsx
+++ src/components/motion/HoverMotion.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import YellowPointLayout from '../layout/YellowPointLayout'
+import YellowPoint from '../ui/YellowPoint'
 
 export default function HoverMotion({
     children,
@@ -10,7 +10,7 @@ export default function HoverMotion({
 }) {
     return (
         <HoverMotionStyled $isnav={isNav}>
-            <YellowPointLayout>{children}</YellowPointLayout>
+            <YellowPoint>{children}</YellowPoint>
         </HoverMotionStyled>
     )
 }
