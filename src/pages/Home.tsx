5b8c448346b855176c2874556ea0264a4a509a1c
KKamjangE <ajm980718@gmail.com>
2024-12-31 13:58:01 +0900

    nav 컴포넌트 제어 위치 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,11 +5,24 @@ import Layout from '@/pages/Layout'
 import ProgressBar from '@/components/ui/ProgressBar'
 import Nav from '@/components/Nav'
 import ScrollTopBtn from '@/components/ui/ScrollTopBtn'
+import { useState } from 'react'
+import Toggle from '@/components/toggle'
 
 export default function Home() {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onOpenToggle = () => {
+        setIsOpen(true)
+    }
+
+    const onCloseToggle = () => {
+        setIsOpen(false)
+    }
+
     return (
         <>
-            <Nav />
+            <Toggle isOpen={isOpen} onOpenToggle={onOpenToggle} />
+            <Nav isOpen={isOpen} onCloseToggle={onCloseToggle} />
             <ProgressBar />
             <Layout>
                 <Intro />
