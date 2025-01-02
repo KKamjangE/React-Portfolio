78c524eaddec54cb506921a8482034b6327df7b8
KKamjangE <ajm980718@gmail.com>
2025-01-02 11:53:34 +0900

    Toggle 컴포넌트 수정

--- src/components/toggle.tsx
+++ src/components/toggle.tsx
@@ -8,21 +8,27 @@ interface ToggleProps {
 
 export default function Toggle({ isOpen, onOpenToggle }: ToggleProps) {
     return (
-        <motion.div animate={{ opacity: isOpen ? 0 : 1 }} transition={{ duration: 0.3 }}>
-            <ToggleStyled onClick={onOpenToggle}>Menu</ToggleStyled>
-        </motion.div>
+        <ToggleStyled
+            onClick={onOpenToggle}
+            animate={{ opacity: isOpen ? 0 : 1 }}
+            transition={{ duration: 0.3 }}
+        >
+            Menu
+        </ToggleStyled>
     )
 }
 
-const ToggleStyled = styled.div`
+const ToggleStyled = styled(motion.div)`
     display: none;
     position: fixed;
     top: 0;
     z-index: 300;
     width: 100%;
+    height: 40px;
     background-color: var(--bg-dark-gray-tablet);
     backdrop-filter: blur(5px);
-    padding: 15px;
+    padding: 0 15px;
+    align-content: center;
     color: #fff;
     cursor: pointer;
     @media screen and (max-width: 1024px) {
